c542e86ed2f4b76c67ab708f93d42e31f3941150
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-01 11:38:49 +0530

    Add files via upload

---
+++ app49.js
@@ -0,0 +1,13 @@
+const  student = {
+    name : "shreyas",
+    age : 20,
+    marks : 85
+};
+
+let student2 = ["shreyas" , 20 , 85];
+
+const item = {
+    price : 100.99,
+    discount : 52,
+    colors : ["red", "pink"]
+};
